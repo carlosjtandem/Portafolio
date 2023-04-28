0a5907d3bb0821e8f1c0532d93215392ebd1b7b8
Carlos Aucancela <carlos.aucancela@resolvit.com>
2023-04-28 11:56:01 -0500

    Add proyecto file

--- src/components/Proyecto.js
+++ src/components/Proyecto.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect,useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { trabajos } from '../data/trabajos';
 
 export const Proyecto = () => {
 
-// recoger parametro por url
-const param= useParams(); 
+    // recoger parametro por url
+    const param = useParams();
+    const [proyecto, setProyecto] = useState({});
 
+    useEffect(() => {
+        let proyecto = trabajos.filter(trabajo => trabajo.id === param.id)
+        setProyecto(proyecto[0]);
+        console.log(proyecto[0]);
+    }, []);
     return (
         <div className='page'>
-            <h1 className='heading'>Proyecto: {param.id}</h1>
+            <h1 className='heading'>Proyecto:{param.id} </h1>
+            <h3>{proyecto.detail}</h3>
         </div>
     )
 }
--- src/data/trabajos.js
+++ src/data/trabajos.js
@@ -1,41 +1,46 @@
 export const trabajos = [
     {
-        'id': 1,
+        'id': '1',
         'identificador': 'Resolvit',
         'nombre': 'internal site - Manage',
         'tecnologias': 'Html5, CSS3, PHP, Rest API',
         'url': 'www.manage.com',
+        'detail': 'Responsable de equipo de desarrollo del proyecto Email Connector. \n Tecnologías usadas Golang, AWS, C#, React.',
         'categorias': 'Desarrollo web'
     },
     {
-        'id': 2,
+        'id': '2',
         'identificador': 'Globant',
         'nombre': 'Cliente Kantar - sitios Norms y TRIM',
         'tecnologias': 'AngularJS, Angular, Html5, CSS3, C#, Azure devops ',
         'url': 'https://trim.kantar.com',
+        'detail': 'Responsable del desarrollo de aplicaciones de alta escalabilidad. <br> Tecnologías usadas Angular, Azure, C#, AngularJS',
         'categorias': 'Desarrollo web'
     },
     {
-        'id': 3,
+        'id': '3',
         'identificador': 'Tandem SAS',
         'nombre': 'Cliente TGI SA ESP',
         'tecnologias': 'Angular, Net core, Html5, CSS3, Hangfire',
         'url': 'intranet',
+        'detail': 'Responsable del desarrollo del sistema de control de contratos. <br> Tecnologías usadas Golang, AWS, C#, React.',
         'categorias': 'Desarrollo web'
     },    {
-        'id': 4,
+        'id': '4',
         'identificador': 'Servientrega',
         'nombre': 'sitio web internos',
         'tecnologias': 'C#, Html, CSS, SQL',
         'url': 'www.facebook.com',
+        'detail': 'Responsable del desarrollo del sistema de control de contratos. <br> Tecnologías usadas Golang, AWS, C#, React.',
         'categorias': 'Desarrollo web'
     },
     {
-        'id': 5,
+        'id': '5',
         'identificador': 'Planmarket',
         'nombre': 'sitio web internos',
         'tecnologias': 'C#, Html, CSS, SQL',
         'url': 'www.facebook.com',
+        'detail': 'Responsable del desarrollo del sistema de control de contratos. <br> Tecnologías usadas Golang, AWS, C#, React.',
         'categorias': 'Desarrollo web'
     }
 ]
